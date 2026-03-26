faac241d18811003b051a2be164b3135d1c58ddc
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-03-26 18:23:57 +0530

    Completed Python basics Day 1 to Day 7

---
+++ all 7 days basics to hard  revison.py
@@ -0,0 +1,13 @@
+# Q1
+#
+# Take a number and print its square
+num = int(input("Enter the number "))
+square = num ** 2
+print(square)
+
+#Q2 Check whether a number is even or odd
+num = int(input("Enter the number "))
+if num % 2==0:
+    print("even ")
+else :
+    print("odd ")
