c94769cd9e01f14573657dee3267dc1238293e63
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-07-15 16:36:58 +0530

    Learned and practiced a new Python program today

---
+++ 01_Basics/wee1_projects/area cal.py
@@ -0,0 +1,6 @@
+length =float(input("Enter the Length "))
+width=float(input("Enter the Width "))
+
+area = length * width
+
+print("\nArea of the Rect is = ",area)
---
+++ 01_Basics/wee1_projects/student details.py
@@ -0,0 +1,8 @@
+name = input("Enter the Student1 name ")
+age = input("Enter the Student1 age ")
+Class = input("Enter the Student1 Class ")
+
+print ("=====Student Details=====")
+print("\nStudent1 name ", name )
+print("\nStudent1 age ", age )
+print("\nStudent1 Class ", Class )
