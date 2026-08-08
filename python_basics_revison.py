2eee9a50a3ef1d4ad1c717385ee98847ee3be4a7
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-08-08 19:54:29 +0530

    .

---
+++ python_basics_revison.py
@@ -0,0 +1,24 @@
+#Variables =simply a name used to store a value
+age = 25
+name ="yashwanth "
+print(age,name )
+
+#Data types what kind of value it is string int float etc
+
+name="Hari "
+age=30
+salary =20000.33
+print(type(name  ),type(age),type(salary))
+print(name,age,salary)
+
+#boolean = only two values True or false
+marks=int(input("enter age"))
+if age <= 10:
+    print("False")
+else:
+    print("True")
+
+#List = Collection of values
+
+numbers =[ 12, 23 ,12, 21 ,2, 1,4 ]
+print(numbers)
