24d0018626d214fb88961860f91933476f560be9
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-08-13 16:12:11 +0530

    git push

--- 02_Practicequestions/primt with user inputs.py
+++ 02_Practicequestions/primt with user inputs.py
@@ -1,7 +1,28 @@
-num = 7
+'''num = 7
 for i in range (2, num):
     if num % i ==0:
         print("not a prime ")
         break
 else:
     print("prime ")
+    '''
+
+num = int(input("Enter the number  "))
+if num <=1:
+    print("not a prime ")
+else:
+    for i in range(2,num):
+        if num%i==0:
+            print("Not a prime ")
+            break
+    else:
+        print("Prime ")
+
+for num in range(2,21):
+    for i in range(2,num):
+        if num %i == 0:
+            break
+    else:
+        print(num)
+
+
