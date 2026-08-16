cdea92787457b01b33d04bb63542213f17f7c600
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-08-16 21:55:40 +0530

    .

--- 02_Practicequestions/primt with user inputs.py
+++ 02_Practicequestions/primt with user inputs.py
@@ -25,6 +25,21 @@ for num in range(2,21):
     else:
         print(num) '''
 
+# count prime numbers
+n = int(input("Enter the number: "))
+count = 0
+num = 2
+while count < n:
+    for i in range(2,num):
+        if num % i == 0:
+            break
+    else:
+        print(num)
+        count+=1
+    num +=1
+
+
+
 
 
 
