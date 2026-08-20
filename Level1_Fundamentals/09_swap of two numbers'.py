c0724671cc2a4839a692cbcae886af58ae7dd02d
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-08-20 16:06:28 +0530

    added swap numbers wihtout using third varible

---
+++ Level1_Fundamentals/09_swap of two numbers'.py
@@ -0,0 +1,11 @@
+num1 =int(input("Enter the number "))
+num2 =int(input("Enter the number "))
+print("\nbefore swapping ")
+print("num1 ", num1)
+print("num2 ", num2)
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print(("\nafter swapping: "))
+print("num1 ", num1)
+print("num2 ", num2)
