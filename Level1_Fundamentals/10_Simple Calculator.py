8bfd3afc7a4629ec7c0d6f00f055398e14005fc5
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-08-21 16:05:53 +0530

    added simple calci

---
+++ Level1_Fundamentals/10_Simple Calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+
+if operator == "+":
+    print("Result:", num1 + num2)
+
+elif operator == "-":
+    print("Result:", num1 - num2)
+
+elif operator == "*":
+    print("Result:", num1 * num2)
+
+elif operator == "/":
+    print("Result:", num1 / num2)
+
+else:
+    print("Invalid operator")
