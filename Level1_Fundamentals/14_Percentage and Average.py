8ca27fa51ddb87583f7493b2678ba747467eee19
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-08-24 15:24:00 +0530

    added Percentage and Average

---
+++ Level1_Fundamentals/14_Percentage and Average.py
@@ -0,0 +1,11 @@
+sub1=int(input("Enter Marks in Subject1 "))
+sub3=int(input("Enter Marks in Subject2 "))
+sub2=int(input("Enter Marks in Subject3 "))
+
+total=sub1+sub2+sub3
+avg = total/3
+percentage =(total/300)*100
+
+print("The total score is = ",total)
+print("The averege of three subjects is = ",avg)
+print("percentage is = ",percentage)
