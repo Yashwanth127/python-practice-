6c7136fe7adaf547822fc015f057c8aade3b7e3b
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-08-30 16:07:14 +0530

    added sum of odd numbers

--- 20 separate Python files/04_Sum of even numbers.py
+++ 20 separate Python files/04_Sum of even numbers.py
@@ -1,6 +1,15 @@
+#adding Even numbers
 n =int(input("Enter n: "))
 total = 0
 for i in range(2,n+1):
     if i%2==0:
-        total = total=+i
+        total = total +i
     print("Sum of even numbers from 2 to", n, "is:", total)
+
+#adding odd numbers
+num =int(input("Enter n: "))
+total = 0
+for i in range(1,n+1):
+    if i%2!=0:
+        total = total +i
+    print("Sum of odd numbers", total)
