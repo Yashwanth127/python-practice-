bd486e0baeb7e003e07956c270de8165d6b8f47d
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-08-30 15:21:33 +0530

    Skip the current iteration and go to the next iteration.

---
+++ 20 separate Python files/break&continue.py
@@ -0,0 +1,12 @@
+# Stop the loop completely.
+for i in range(0, 10):
+    if i == 3:
+        break
+
+    print(i)
+
+#It(continue) Skips the  current Iteration
+for i in range(1,20):
+    if i==15:
+        continue
+    print(i)
--- 20 separate Python files/break.py
+++
@@ -1,6 +0,0 @@
-# Stop the loop completely.
-for i in range(1, 10):
-    if i == 10:
-        break
-
-    print(i)
